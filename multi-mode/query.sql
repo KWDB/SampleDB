ccbb8db40bc3ec369b0d73d44ca78cbeea9a86a8
ruanzhijin <ruanzhijin@inspur.com>
2025-11-19 11:35:26 +0800

    update multi-mode

--- multi-mode/query.sql
+++ multi-mode/query.sql
@@ -1,80 +1,80 @@
-SELECT si.station_name,
-       COUNT(t.measure_value),
-       AVG(t.measure_value)
-FROM pipec_r.station_info si,              
-     pipec_r.workarea_info wi,             
-     db_pipec.t_point t                    
-WHERE wi.work_area_sn = si.work_area_sn    
-  AND si.station_sn = t.station_sn         
-  AND t.measure_type = 1                   
-  AND t.measure_value > 50                 
-GROUP BY si.station_name
-HAVING COUNT(t.measure_value) > 3
-ORDER BY si.station_name;
+SELECT si.site_name,
+       COUNT(t.monitor_value),
+       AVG(t.monitor_value)
+FROM monitor_r.site_info si,              
+     monitor_r.region_info wi,             
+     db_monitor.t_monitor_point t                    
+WHERE wi.region_id = si.region_id    
+  AND si.site_id = t.site_id         
+  AND t.monitor_type = 1                   
+  AND t.monitor_value > 50                 
+GROUP BY si.site_name
+HAVING COUNT(t.monitor_value) > 3
+ORDER BY si.site_name;
 
-SELECT wi.work_area_name,
-       si.station_name,
-       t.measure_type,
-       time_bucket(t.k_timestamp, '10s') AS timebucket,
-       AVG(t.measure_value) AS avg_value,
-       MAX(t.measure_value) AS max_value,
-       MIN(t.measure_value) AS min_value,
-       COUNT(t.measure_value) AS number_of_value
-FROM pipec_r.station_info si,
-     pipec_r.workarea_info wi,
-     pipec_r.pipeline_info li,
-     pipec_r.point_info pi,
-     db_pipec.t_point t
-WHERE li.pipeline_sn = pi.pipeline_sn
-  AND pi.station_sn = si.station_sn
-  AND si.work_area_sn = wi.work_area_sn
-  AND t.point_sn = pi.point_sn
+SELECT wi.region_name,
+       si.site_name,
+       t.monitor_type,
+       time_bucket(t.k_collect_time, '10s') AS timebucket,
+       AVG(t.monitor_value) AS avg_value,
+       MAX(t.monitor_value) AS max_value,
+       MIN(t.monitor_value) AS min_value,
+       COUNT(t.monitor_value) AS number_of_value
+FROM monitor_r.site_info si,
+     monitor_r.region_info wi,
+     monitor_r.pipeline_info li,
+     monitor_r.point_base_info pi,
+     db_monitor.t_monitor_point t
+WHERE li.pipeline_id = pi.pipeline_id
+  AND pi.site_id = si.site_id
+  AND si.region_id = wi.region_id
+  AND t.point_id = pi.point_id
   AND li.pipeline_name = 'Pipe_3'
-  AND wi.work_area_name in ('Area_8', 'Area_12', 'Area_16')
-  AND t.k_timestamp >= '2023-08-01 01:00:00'
-GROUP BY wi.work_area_name,
-         si.station_name,
-         t.measure_type,
+  AND wi.region_name in ('Area_8', 'Area_12', 'Area_16')
+  AND t.k_collect_time >= '2023-08-01 01:00:00'
+GROUP BY wi.region_name,
+         si.site_name,
+         t.monitor_type,
          timebucket;
 
-	SELECT
-    time_bucket(t.k_timestamp, '1h') AS timebucket,
-    s.work_area_sn,
-    w.work_area_name,
+SELECT
+    time_bucket(t.k_collect_time, '1h') AS timebucket,
+    s.region_id,
+    w.region_name,
     pinfo.pipeline_name,
-    COUNT(t.k_timestamp) AS measurement_count,
-    SUM(t.measure_value) AS total_measure_value,
-    AVG(t.measure_value) AS avg_measure_value
+    COUNT(t.k_collect_time) AS measurement_count,
+    SUM(t.monitor_value) AS total_measure_value,
+    AVG(t.monitor_value) AS avg_measure_value
 FROM
-    db_pipec.t_point t,        -- 45M
-    pipec_r.station_info s,    -- 436
-    pipec_r.workarea_info w,   -- 41
-    pipec_r.pipeline_info pinfo  -- 26
+    db_monitor.t_monitor_point t,        
+    monitor_r.site_info s,    
+    monitor_r.region_info w,   
+    monitor_r.pipeline_info pinfo  
 WHERE
-    t.work_area_sn = s.work_area_sn    -- 41, 41
-    AND t.pipeline_sn = pinfo.pipeline_sn    -- 21, 41
-    AND s.work_area_sn = w.work_area_sn    -- 41, 41
-    AND t.k_timestamp BETWEEN '2023-01-04 14:31:00' AND '2030-01-04 14:31:00'    -- 2M/45M
+    t.region_id = s.region_id    
+    AND t.pipeline_id = pinfo.pipeline_id    
+    AND s.region_id = w.region_id    
+    AND t.k_collect_time BETWEEN '2023-01-04 14:31:00' AND '2030-01-04 14:31:00'    
 GROUP BY
-    timebucket, s.work_area_sn, w.work_area_name, pinfo.pipeline_name
+    timebucket, s.region_id, w.region_name, pinfo.pipeline_name
 ORDER BY
-    timebucket, s.work_area_sn
+    timebucket, s.region_id
 LIMIT 100;
 
-SELECT wi.work_area_name,
-       si.station_name,
-       t.measure_type,
-       time_bucket(t.k_timestamp, '5s') as timebucket,
-       avg(t.measure_value)
-FROM pipec_r.point_info pi,           -- 41
-     pipec_r.pipeline_info li,        -- 41
-     pipec_r.workarea_info wi,        -- 41
-     pipec_r.station_info si,         -- 436
-     db_pipec.t_point t               -- 45M
-WHERE pi.pipeline_sn = li.pipeline_sn
-  AND pi.station_sn = si.station_sn
-  AND si.work_area_sn = wi.work_area_sn -- 41, 41
-  AND pi.point_sn = t.point_sn      -- 436, 401
-  AND li.pipeline_name = 'Pipe_9'    -- 1/26
-  AND wi.work_area_name = 'Area_7' -- 1/41
-GROUP BY wi.work_area_name, si.station_name, t.measure_type, timebucket;
+SELECT wi.region_name,
+       si.site_name,
+       t.monitor_type,
+       time_bucket(t.k_collect_time, '5s') as timebucket,
+       avg(t.monitor_value)
+FROM monitor_r.point_base_info pi,           
+     monitor_r.pipeline_info li,        
+     monitor_r.region_info wi,        
+     monitor_r.site_info si,        
+     db_monitor.t_monitor_point t               
+WHERE pi.pipeline_id = li.pipeline_id
+  AND pi.site_id = si.site_id
+  AND si.region_id = wi.region_id 
+  AND pi.point_id = t.point_id      
+  AND li.pipeline_name = 'Pipe_9'    
+  AND wi.region_name = 'Area_7' 
+GROUP BY wi.region_name, si.site_name, t.monitor_type, timebucket;
